a12bd67b9a6dbea0a2df890f11ed338145119414
helw150 <wbh230@nyu.edu>
2018-05-12 00:19:32 +0400

    Component out git card

---
+++ src/components/GithubFeed/GithubCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+class GithubCard extends React.Component {
+  render() {
+    return (
+      <div className="github-post" style={{ height: "33%" }}>
+        <h3
+          style={{
+            fontSize: "2.5vh",
+            margin: "auto",
+            marginTop: 8,
+            overflow: "hidden",
+            whiteSpace: "nowrap",
+            textOverflow: "ellipsis"
+          }}
+        >
+          <a href={this.props.repo.url}>{this.props.repo.name}</a>
+        </h3>
+        <div
+          style={{
+            fontSize: "1.5vh",
+            overflow: "hidden",
+            whiteSpace: "nowrap",
+            textOverflow: "ellipsis",
+            borderBottom: "1px dotted #BDBDBD"
+          }}
+        >
+          <div
+            style={{
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis"
+            }}
+          >
+            {this.props.repo.description}
+          </div>
+          {this.props.repo.languages.edges.map(language => (
+            <span
+              style={{
+                color: language.node.color,
+                marginRight: "3px",
+                filter: "brightness(65%)"
+              }}
+            >
+              {language.node.name}
+            </span>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GithubCard;
--- src/components/GithubFeed/GithubFeed.jsx
+++ src/components/GithubFeed/GithubFeed.jsx
@@ -1,58 +1,14 @@
 import React from "react";
 import Link from "gatsby-link";
-import _ from "lodash";
+
+import GithubCard from "./GithubCard";
 
 class GithubFeed extends React.Component {
   render() {
     const repos = this.props.repos.edges.map(repo => repo.node);
     return (
       <div className="github-feed">
-        {repos.map(repo => (
-          <div className="github-post" style={{ height: "33%" }}>
-            <h3
-              style={{
-                fontSize: "2.5vh",
-                margin: "auto",
-                marginTop: 8,
-                overflow: "hidden",
-                whiteSpace: "nowrap",
-                textOverflow: "ellipsis"
-              }}
-            >
-              <a href={repo.url}>{repo.name}</a>
-            </h3>
-            <div
-              style={{
-                fontSize: "1.5vh",
-                overflow: "hidden",
-                whiteSpace: "nowrap",
-                textOverflow: "ellipsis",
-                borderBottom: "1px dotted #BDBDBD"
-              }}
-            >
-              <div
-                style={{
-                  overflow: "hidden",
-                  whiteSpace: "nowrap",
-                  textOverflow: "ellipsis"
-                }}
-              >
-                {repo.description}
-              </div>
-              {repo.languages.edges.map(language => (
-                <span
-                  style={{
-                    color: language.node.color,
-                    marginRight: "3px",
-                    filter: "brightness(65%)"
-                  }}
-                >
-                  {language.node.name}
-                </span>
-              ))}
-            </div>
-          </div>
-        ))}
+        {repos.map(repo => <GithubCard repo={repo} />)}
       </div>
     );
   }
